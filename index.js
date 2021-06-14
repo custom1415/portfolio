44b1e59b029c89f21f3964ce1e6e3e8393352e3e
custom1415 <80276016+custom1415@users.noreply.github.com>
2021-06-14 19:26:32 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const barContainer = document.querySelector(".bar-container");
 const toggleSideNav = () => {
   document.querySelector(".sidenav").classList.toggle("active");
-barContainer.classList.toggle("change")
-
-}
+  barContainer.classList.toggle("change");
+};
 
 barContainer.addEventListener("click", toggleSideNav);
+
+const loader = document.querySelector(".lds-heart");
+const image = document.querySelectorAll("img");
+const section = document.querySelectorAll("section");
+console.log(section);
+const showLoader = () => {
+  if (!image.completed) {
+    section.forEach((s) => (s.style.display = "none"));
+    console.log("hi");
+  }
+
+  loader.style.display = "none";
+  section.forEach((s) => (s.style.display = "grid"));
+};
+
+showLoader();
