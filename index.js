7bd5f7deb5b96b8e3f70997b224635ea78b87f3a
custom1415 <80276016+custom1415@users.noreply.github.com>
2021-06-16 18:42:09 +0545

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ const toggleSideNav = () => {
   const sidenav = document.querySelector(".sidenav");
   sidenav.classList.toggle("active");
   barContainer.classList.toggle("change");
-  deform(cardOne);
-  deform(cardTwo);
-  if (sidenav.classList.contains("active")) {
-    content.removeEventListener("click", showModal);
-  }
-  else{
-    
-    content.addEventListener("click", showModal);
-  }
 };
 
 barContainer.addEventListener("click", toggleSideNav);
@@ -31,36 +22,6 @@ const showLoader = () => {
 };
 window.addEventListener("load", showLoader);
 
-console.log(msgImg.getBoundingClientRect());
-
-const content = document.querySelector(".content");
-const container = document.querySelector(".container");
-
-const transform = (cardNum) => {
-  cardNum.style.transform = "scale(1)";
-  cardNum.style.opacity = "1";
-  console.log('hi');
-};
-const deform = (cardNum) => {
-  cardNum.style.transform = "scale(0)";
-  cardNum.style.opacity = "0";
-};
-const showModal = (e) => {
-  console.log(e);
-  shouldModalBeShown = true;
-  if (e.target.className === "card-one") {
-    return;
-  }
-  console.log(e.target.offsetWidth / 2, e.offsetX);
-  if (e.offsetX < e.target.offsetWidth / 2) {
-    transform(cardOne);
-  } else {
-    deform(cardOne);
-  }
-  if (e.offsetX > e.target.offsetWidth / 2) {
-    transform(cardTwo);
-  } else {
-    deform(cardTwo);
-  }
-};
-container.addEventListener("click", showModal);
+const aboutMe = document.querySelector('.about-me')
+const chevDown = document.querySelector('.fas')
+chevDown.addEventListener('click',()=>aboutMe.scrollIntoView({behavior: "smooth"}))
